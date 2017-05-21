6e7bf002d35cda43b0708d9c3c3b5a88aec51dc9
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-05-21 04:38:01 -0400

    adds starter html and javascript code

---
+++ app.js
@@ -0,0 +1,7 @@
+//The logic behind the gifs!
+
+var topics = ["freddy krueger","michael myers","jack torrance","hannibal lecter","gollum"];
+
+for (i=0; i < topics.length; i++) {
+    $("#buttons").append("<button id=" + topics[i] + ">" + topics[i] +"</button>");  
+}
