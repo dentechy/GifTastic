b0a778b55727db292a0c96dc3929db0980b83acd
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-05-21 14:15:16 -0400

    adds finishing touches

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 //The logic behind the gifs!
 
-var topics = ["freddy krueger","michael myers","jack torrance","hannibal lecter","gollum"];
+var topics = ["darth vader","joker","venom","voldemort","gollum"];
 
 for (i=0; i < topics.length; i++) {
-    $("#buttons").append("<button id=" + topics[i] + ">" + topics[i] +"</button>");  
+    $("#buttons").append("<button data-villain=" + topics[i] + ">" + topics[i] +"</button>");  
 }
 
-$("#" + topics[i]).on("click",function() {
+
+$("button").on("click",function() {
+
+    var villain = $(this).attr("data-villain");
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + villain + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .done(function(response) {
+        console.log(response);
+    
+        var results = response.data;
+
+        for (var z = 0; z < results.length; z++) {
+            var villainDiv = $("<div>");
+
+            var pRating = $("<p>").text("Rating: " + results[z].rating);
+
+            var villainImage = $("<img>");
+
+            //place here the src attribute for the stills, later an if else statement for data state
+            villainImage.attr("src", results[z].images.fixed_height_still.url);
+            villainImage.attr("class", "villainGif");
+            villainImage.attr("data-state", "still");
+            villainImage.attr("data-still", results[z].images.fixed_height_still.url);
+            villainImage.attr("data-animate", results[z].images.fixed_height.url);
+
+            villainDiv.append(pRating);
+            villainDiv.append(villainImage);
+
+            $("#gifs").prepend(villainDiv);
+        }
+
+        $(".villainGif").on("click", function() {
+            var state = $(this).attr("data-state");
 
 
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        })
+
+    });
+
+});
 
 
-
-
-
-
-
-
-})
